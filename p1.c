fed4281eafe56113ddc32fed6d8b9f7cf9493e2f
Igor Canadi <igor.canadi@gmail.com>
2013-02-24 14:21:43 -0600

    final changes to p1, hopefully

--- p1.c
+++ p1.c
@@ -7,29 +7,29 @@
 #include <sys/stat.h>
 #include "rdtsc.h"
 
-#define MIN_READ_SZ	1024
-#define MAX_READ_SZ	(10*1024*1024)	// Maximum read size, bytes
+#define MIN_READ_SZ    512*1024
+#define MAX_READ_SZ    (30*1024*1024) // Maximum read size, bytes
 #define MAX_NUM_READS 1024*1024
-#define NUM_RUNS    5
-#define STEP_TRANSFORM(x)	x += MIN_READ_SZ
+#define NUM_RUNS    20
+#define STEP_TRANSFORM(x)    x -= MIN_READ_SZ
 
 // Vary read size 
 unsigned int runExp(const char *fName, uint64_t *res){
 
-	int fd;	// File descriptor 
-	uint64_t t1, t2;
+    int fd;    // File descriptor 
+    uint64_t t1, t2;
     int read_out;
 
-	off64_t offset;
-	off64_t size;
+    off64_t offset;
+    off64_t size;
 
-	char *buf = (char *)malloc(MAX_READ_SZ);
-	unsigned int readSize;
-	unsigned int index;
+    char *buf = (char *)malloc(MAX_READ_SZ);
+    unsigned int readSize;
+    unsigned int index;
 
-	// Perform random read
-	for (readSize = MIN_READ_SZ, index = 0; readSize < MAX_READ_SZ && index < MAX_NUM_READS; STEP_TRANSFORM(readSize), index++){
-	    system("sync; echo 3 >| /proc/sys/vm/drop_caches");
+    // Perform random read
+    for (readSize = MAX_READ_SZ, index = 0; readSize >= MIN_READ_SZ && index < MAX_NUM_READS; STEP_TRANSFORM(readSize), index++){
+        system("sync; echo 3 >| /proc/sys/vm/drop_caches");
         if ( (fd = open(fName, O_RDONLY|O_LARGEFILE)) < 0){
             perror("fopen error");
             exit(-1);
@@ -39,53 +39,53 @@ unsigned int runExp(const char *fName, uint64_t *res){
             perror("read error");
             exit(-1);
         }
-		
-		// Read from random X * read Size
-		offset = random() % (size - readSize);
-		
-		offset = offset - (offset % readSize);
-		lseek(fd, offset, SEEK_SET);
+        
+        // Read from random X * read Size
+        offset = ((long long)random() * 100 + random()) % (size - readSize);
+        
+        offset = offset - (offset % readSize);
+        lseek(fd, offset, SEEK_SET);
 
-		t1 = rdtsc_start();
+        t1 = rdtsc_start();
         read_out = read(fd, buf, readSize);
-		t2 = rdtsc_end();
+        t2 = rdtsc_end();
         if (read_out != readSize) {
-			printf("# reads %d\n", index);
-			break;	// Read i bytes
+            printf("# reads %d\n", index);
+            break;    // Read i bytes
         }
 
-		if (res[index] == 0 || (t2-t1) < res[index]){
-			res[index] = t2-t1;
-		}
-        close(fd);	
-	}
+        if (res[index] == 0 || (t2-t1) < res[index]){
+            res[index] = t2-t1;
+        }
+        close(fd);    
+    }
 
     free(buf);
 
-	return index;
+    return index;
 }
 
 int main(int argc, char *argv[]){
-	uint64_t *intervals = calloc(MAX_NUM_READS, sizeof(uint64_t));
+    uint64_t *intervals = calloc(MAX_NUM_READS, sizeof(uint64_t));
 
-	unsigned int i, readSize;
-	unsigned int maxInd;
-	
-	if (argc != 2){
-		printf("Usage: p1 working_file\n");
-		exit(-1);
-	}
+    unsigned int i, readSize;
+    unsigned int maxInd;
+    
+    if (argc != 2){
+        printf("Usage: p1 working_file\n");
+        exit(-1);
+    }
 
-	// Run experiment
-	for(i = 0; i < NUM_RUNS; i++){
-		maxInd = runExp(argv[1], intervals);
-	}
+    // Run experiment
+    for(i = 0; i < NUM_RUNS; i++){
+        maxInd = runExp(argv[1], intervals);
+    }
 
-	// Print results
-	for(i = 0, readSize=MIN_READ_SZ; i < maxInd; i++, STEP_TRANSFORM(readSize)){
-		printf("%u %lf\n", readSize, (double)intervals[i]/readSize);
-	}
-	
-	return 0;
+    // Print results
+    for(i = 0, readSize=MAX_READ_SZ; i < maxInd; i++, STEP_TRANSFORM(readSize)){
+        printf("%u %lf\n", readSize, (double)intervals[i]/readSize);
+    }
+    
+    return 0;
 
 }
