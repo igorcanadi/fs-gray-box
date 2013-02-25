db17f61fc474ab1af21339c8abe63327aaac2421
Igor Canadi <igor.canadi@gmail.com>
2013-02-24 19:26:28 -0600

    data + convert rename

--- convert_units.py
+++ convert_units.py
@@ -5,6 +5,6 @@ if len(sys.argv) < 2:
 
 a = open(sys.argv[1]).read().split('\n')[:-1]
 a = map(lambda x: map(float, x.split(' ')), a)
-a = map(lambda x: (x[0], x[1] / 2.4), a)
+a = map(lambda x: (x[0] * 512.0 / 1024, x[1] / 2400.0), a)
 a = map(lambda x: " ".join(map(str, x)), a)
 sys.stdout.write("\n".join(a) + "\n")
