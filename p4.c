c877739337ce9c2dd1cbd2004e2545521cd83052
Igor Canadi <igor.canadi@gmail.com>
2013-03-02 19:09:22 -0600

    Completely new p4, new results, new text

--- p4.c
+++ p4.c
@@ -7,60 +7,68 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#define TEST_FILE "/scratch/.tmpfiledump"
-#define BLOCK_SIZE 128
-#define FROM_SIZE 1
-#define TO_SIZE 10000
+#define MB 1024*1024LL
+#define BLOCK_SIZE 4096
+#define TO_SIZE 4000
 #define RUNS 5
+#define PUT_FIRST 1
 
-void put_junk(char *buffer) {
-    int i;
+uint64_t results[TO_SIZE + 1];
+
+int main() {
+    int err;
+    uint64_t i, r;
+    uint64_t tmp, cur_time;
+    char *buffer;
+    int fd;
+    FILE *f;
+    char test_file[] = "/scratch/dumpxx";
+
+    buffer = (char *)malloc(BLOCK_SIZE);
     for (i = 0; i < BLOCK_SIZE; ++i) {
         buffer[i] = rand() % 128;
     }
-}
 
-int main() {
-    int r, t;
-    uint64_t i;
-    char buffer[BLOCK_SIZE];
-    uint64_t tmp, cur_time;
-    uint64_t results[TO_SIZE+1] = {0};
-    int fd;
-   
     for (r = 0; r < RUNS; ++r) {
-        unlink(TEST_FILE);
-        fd = open(TEST_FILE, O_WRONLY | O_SYNC | O_CREAT);
-        if (fd == -1 || fsync(fd) == -1) {
-            fprintf(stderr, "syserror\n");
-            return 1;
-        }
-        for (i = FROM_SIZE; i <= TO_SIZE; ++i) {
-            put_junk(buffer);
+        test_file[13] = (r/10) % 10 + '0';
+        test_file[14] = r % 10 + '0';
+        unlink(test_file);
+        for (i = 1; i <= TO_SIZE; ++i) {
+            fd = open(test_file, O_WRONLY | O_SYNC | O_CREAT);
+            fsync(fd);
+            if (fd == -1) {
+                fprintf(stderr, "syserror1\n");
+                return 1;
+            }
+            err = lseek(fd, 0, SEEK_END);
+            if (err == -1) {
+                fprintf(stderr, "syserror2\n");
+                return 1;
+            }
             tmp = rdtsc_start();
-            // measure only one byte
-            t = write(fd, buffer, 1);
+            err = write(fd, buffer, PUT_FIRST);
             cur_time = rdtsc_end() - tmp;
+            if (err != PUT_FIRST) {
+                fprintf(stderr, "syserror3\n");
+                return 1;
+            }
+            if (BLOCK_SIZE-PUT_FIRST) {
+                err = write(fd, buffer+1, BLOCK_SIZE-PUT_FIRST);
+                if (err != BLOCK_SIZE-PUT_FIRST) {
+                    fprintf(stderr, "syserror3\n");
+                    return 1;
+                }
+            }
+            close(fd);
+
             if (r == 0 || results[i] > cur_time) {
                 results[i] = cur_time;
             }
-            if (t != 1) {
-                fprintf(stderr, "syserror\n");
-                return 1;
-            }
-
-            // put in the rest
-            t = write(fd, buffer+1, BLOCK_SIZE-1);
-            if (t != BLOCK_SIZE-1) {
-                fprintf(stderr, "syserror\n");
-                return 1;
-            }
         }
-        close(fd);
     }
 
-    for (i = FROM_SIZE; i <= TO_SIZE; ++i) {
-        printf("%llu %llu\n", i, results[i]);
+    for (i = 1; i <= TO_SIZE; ++i) {
+        printf("%llu %llu\n", i*BLOCK_SIZE, results[i]);
     }
 
     return 0;
